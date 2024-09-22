c2503da76e8a0abd52ba6f879fc0f8c98c517544
Jorge Leandro <jorgejgleandro@gmail.com>
2024-09-21 23:30:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,177 @@
+# *Brazilian Jiu-Jitsu Training Log - Frontend*
+
+Esta é uma aplicação web que permite o acompanhamento objetivo do progresso do aprendizado de técnicas de Jiu-Jitsu, um pequeno projeto que constitui o MVP a ser apresentado à disciplina **Arquitetura de Software** do curso de **Pós-graduação em Engenharia de Software - PUC-Rio**
+
+## Descrição
+
+<img src="./src/img/positions.jpg" alt="Brazilian Jiu-Jitsu Training Image" title="Brazilian Jiu-Jitsu Training Image" width="300" style="float:left; margin:0px 20px"/>
+
+Artes Marciais são sistemas de luta que codificam e organizam movimentos visando ao combate corpo-a-corpo, presentes em diversas culturas e tradições milenares.
+No mundo moderno, a prática de artes marciais ganhou contornos diferentes das motivações simplesmente militares que as originaram, tornando-se um negócio bilionário. Em grande medida, essa popularização se deve aos benefícios para a saúde física, mental e  possíveis benefícios para autodefesa.
+Não obstante as diferenças de origens, princípios e técnicas entre esses sistemas, de alguma forma todos eles avaliam seus praticantes periodicamente, a fim de posicioná-los conforme nível técnico. Os critérios dessas avaliações podem incluir tempo de prática, frequência, resultados em torneios e desempenho nas sessões de treino, além de outros aspectos.
+Em particular, a análise de desempenho e domínio técnico pode ser subjetiva, a partir da perspectiva de algum instrutor. O aluno pode sentir-se trilhando uma jornada obscura, com dificuldades para reconhecer os próprios avanços, o que pode ser bastante desmotivante.
+Nesse contexto, seria muito interessante que houvesse um sistema objetivo, quantitativo, para acompanhamento do desempenho de praticantes durante sessões de treino, que pudesse ser conduzido pelo próprio praticante ou pelo professor de uma escola.
+A presente ferramenta se propõe a preencher essa lacuna, possibilitando não somente o registro gradual das técnicas aprendidas por um praticante, mas também as respectivas tentativas de aplicações, como métrica de progresso na maestria das técnicas. Essas tentativas podem culminar em sucesso ou insucesso, seja no papel de ataque ou defesa do praticante em questão.
+Apesar do Jiu-Jitsu Brasileiro (*Brazilian Jiu Jitsu*) ter sido aqui eleito como caso de uso, considerando sua popularidade e a tradição de prática de lutas em todas as sessões de treino, em princípio, esse sistema poderia ser aplicado a qualquer modalidade que permita contar os números de sucessos e insucessos em aplicações de técnicas. A contagem poderia ser efetuada pelo próprio praticante, com base em suas recordações do treinamento, pelo professor assistindo a um pequeno grupo de alunos ou por um sistema de Visão Computacional baseado em vídeo com Estimação de Pose.
+
+---
+Veja o [vídeo de apresentação](https://youtu.be/JNw1kgE5TsQ) no Youtube.
+
+---
+
+<img src="mvp-3-diagram.png" alt="Arquitetura do BJJ Training Log" title="Brazilian Jiu-Jitsu Training Image" width="600" class="center"/>
+
+<center> Figura: Arquitetura do BJJ Training Log </center>
+
+---
+## Baixar repositório
+
+Faça uma cópia desse repositório para sua máquina local, preferencialmente com o comando abaixo.
+```
+git clone git@github.com:jorgejgleandro/bjj-training-log-arq-soft-frontend.git
+```
+
+Acesse a **branch** *main* do repositório:
+
+```
+git checkout main
+```
+
+---
+## Instalação e Execução
+
+Vale salientar que, para executar a aplicação com esse componente  *frontend*, é necessário que o respectivo componente *backend* esteja em execução.
+
+---
+### Modo desenvolvimento: Execução
+Feito isso, abra o arquivo *index.html* no seu browser.
+
+---
+### Modo produção via Docker: Instalação
+Certifique-se de ter o [Docker](https://docs.docker.com/engine/install/) devidamente instalado e em execução em sua máquina.
+
+Pelo terminal, acesse o diretório que contém o Dockerfile, um nível acima do diretório src/, onde encontram-se os arquivos do frontend
+
+Construa a imagem Docker mediante o seguinte comando, **como administrador**:
+
+```
+$ docker build -t nome_da_sua_imagem .
+```
+
+### Modo produção via Docker: Execução
+
+Execute o container, execute o seguinte comando, **como administrador**, seguinte o comando:
+
+```
+$ docker run -d -p 9000:80 nome_da_sua_imagem
+```
+
+Uma vez executando, para acessar o front-end, basta abrir o [http://localhost:9000/#/](http://localhost:9000/#/) no navegador.
+
+Para obter a implementação da **API**, dirija-se ao repositório do componente [*backend*](https://github.com/jorgejgleandro/bjj-log-app-arq-soft-api) e siga aquelas instruções.
+
+
+## APIs Externas Públicas
+
+Três APIs externas foram utilizadas como componentes nesta aplicação, a saber:
+
+1. [OpenAI API](https://openai.com/api/)
+
+Para geração automática de descrição de técnica ao adicioná-la, dado um termo para o nome da técnica, foi usado o modelo [GPT-4o](https://platform.openai.com/docs/models/gpt-4o) da [OpenAI API](https://openai.com/api/), o mais avançado modelo multimodal para geração de texto dessa API, com janela de contexto de *128.000* tokens, e último *checkpoint* de treinamento datado de *Outubro de 2023*.
+
+    1.1. Termos de Uso
+
+    O documento legal que dispõe sobre os termos de uso pode ser encontrado em:
+
+[Terns of Use](https://openai.com/policies/terms-of-use/)
+
+    1.2. Cadastro
+
+    Para execução dessa aplicação, não é necessário cadastro na OpenAI, desde que o usuário esteja em posse de alguma chave **OpenAI_API_KEY**, gerada e fornecida por um assinante dos serviços dessa API.
+
+    Se a chave de API for fornecida, copie seu valor para a variável correspondente no cabeçalho do código do arquivo scripts.js:
+```
+const OpenAI_API_KEY='PROVIDED_OPENAI_API_KEY'
+```
+
+    Caso a chave de API não seja fornecida, siga os passos:
+
+        * Crie uma conta na OpenAI: Acesse o site oficial da OpenAI e faça login com sua conta existente ou crie uma nova conta, se necessário.
+        * Acesse suas chaves de API:
+            * Após fazer login, clique na foto do seu perfil no canto superior direito.
+            * Selecione “Exibir chaves de API”.
+        * Crie uma nova chave secreta:
+            * Clique no botão “Criar nova chave secreta”.
+            * Copie e cole a chave de API como valor para a respectiva variável no arquivo scripts.js: const OpenAI_API_KEY=''
+
+    ou consulte a documentação abaixo
+
+[Developer Quickstart](https://platform.openai.com/docs/quickstart)
+
+    1.3. Rota utilizada
+
+        https://api.openai.com/v1/chat/completions
+
+2. [Youtube Data API](https://developers.google.com/youtube/v3?hl=pt-br)
+
+Para obter o link para um vídeo relacionado ao termo para o nome da técnica, ao adicioná-la, foi usada a [Youtube Data API](https://developers.google.com/youtube/v3/docs/search/list?hl=pt-br)
+
+    2.1. Termos de Serviço
+
+    O documento legal que dispõe sobre os termos de serviços pode ser encontrado em:
+
+> [Youtube API Services Terms of Service](https://developers.google.com/youtube/terms/api-services-terms-of-service)
+
+    2.2. Cadastro
+
+        Para execução dessa aplicação, não é necessário cadastro na Youtube API, desde que o usuário possua alguma chave **Youtube_API_KEY**, gerada e fornecida por um assinante dos serviços dessa API.
+
+            Se a chave de API for fornecida, copie seu valor para a variável correspondente no cabeçalho do código do arquivo scripts.js:
+```
+const Youtube_API_KEY='PROVIDED_Youtube_API_KEY'
+```
+
+        Caso a chave de API não seja fornecida, siga os passos:
+
+            * Crie um projeto no Console de APIs do Google:
+                * Acesse o Console de APIs do Google.
+                * Crie um novo projeto ou selecione um existente.
+
+            * Ative a API do YouTube para o projeto:
+                * No painel do projeto, clique em “Ativar APIs e serviços”.
+                * Pesquise por “YouTube Data API” e ative-a.
+
+            * Crie as credenciais necessárias:
+                * Acesse a seção “Credenciais” no painel do projeto.
+                * Clique em “Criar credenciais” e escolha “Chave de API”.
+                * crie uma chave de API
+                * Copie e cole a chave de API como valor para a respectiva variável no arquivo scripts.js: const Youtube_API_KEY=''
+
+        ou consulte a documentação abaixo
+
+[Visão Geral da API Youtube Data](https://developers.google.com/youtube/v3/getting-started?hl=pt-br)
+
+    2.3. Rota utilizada
+
+        https://www.googleapis.com/youtube/v3
+
+3. viaCEP API
+
+    3.1. Licença
+
+        Não se aplica
+
+    3.2. Cadastro
+
+        Não se aplica
+
+    3.3. Rota utilizada
+
+        https://viacep.com.br/ws/${cep}/json/
+
+
+## *Prompt* empregado:
+
+> `Descreva a posição do Brazilian Jiu-Jitsu denominada ${termDescribe}. A descrição deve ser subdividida em: Princípio:, Posição Inicial: (e.g Guarda, Raspagem, Montada), Execução: (passos numerados), Variações:, Dicas Importantes: e Lembre-se:`
+
+, onde **termDescribe** é um *placeholder* para o termo com o nome da técnica fornecido pelo usuário.
---
+++ src/scripts.js
@@ -0,0 +1,439 @@
+const OpenAI_HOST=`https://api.openai.com/v1/chat/completions`
+const OpenAI_API_KEY=''
+const Youtube_API_KEY=''
+
+const loader = document.getElementById("loader");
+
+/*
+  --------------------------------------------------------------------------------------
+  COMPONENTE EXTERNO - OPENAI
+  Função genérica para obter um prompt para a descrição do termo dado
+  --------------------------------------------------------------------------------------
+*/
+const getPrompt = (termDescribe) => {
+  
+  const prompt = `Descreva a posição do Brazilian Jiu-Jitsu denominada ${termDescribe}. A descrição deve ser subdividida em: Princípio:, Posição Inicial: (e.g Guarda, Raspagem, Montada), Execução: (passos numerados), Variações:, Dicas Importantes: e Lembre-se:`
+  
+  console.log(`in getPrompt: ${prompt}`);
+
+  return prompt;
+}
+
+/*
+  -------------------------------------------------------------------------------------------------------------
+  COMPONENTE EXTERNO - OPENAI
+  Função genérica para obter texto gerado por um LLM via requisição GET, usando a API da OpenAI, dado um prompt
+  -------------------------------------------------------------------------------------------------------------
+*/
+const generateText = async (prompt) => {
+  const url = OpenAI_HOST;
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${OpenAI_API_KEY}`
+    },
+    body: JSON.stringify({
+      model: 'gpt-4o',
+      messages: [{role: "user", content: `${prompt}`}],
+      temperature: 0.7,
+      max_tokens: 4000,
+      n: 1,
+      stop: null,
+    }),
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(`data: ${data}`);
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+/*
+  -----------------------------------------------------------------------------------------------------
+  COMPONENTE EXTERNO YOUTUBE
+  Função genérica para obter lista de videos via requisição GET, usando a API do Youtube, dado um termo
+  -----------------------------------------------------------------------------------------------------
+*/
+const fetchVideos = async (query, maxResults) => {
+  const terms = `${query}+brazilian+jiu-jitsu+bjj`;
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${terms}&key=${Youtube_API_KEY}&maxResults=${maxResults}`
+
+  try{
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok){
+      throw new Error(`Error ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(`Video data: ${JSON.stringify(data)}`);
+    return data;
+  }
+  catch(error){
+    console.error('Error:', error);
+  }
+}
+
+/*
+  -----------------------------------------------------------------------------------------------------------------
+  COMPONENTE EXTERNO VIACEP
+  Função genérica para obter endereço como string via requisição GET, usando a API do ViaCEP, dado um número de CEP
+  -----------------------------------------------------------------------------------------------------------------
+*/
+const getCEP = async (cep) => {
+  try {
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error('Error:', error);
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função genérica para obter uma lista existente do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getList = async (route_name, myTable) => {
+  let url = 'http://127.0.0.1:5000/' + route_name;
+  fetch(url, {
+    method: 'get',
+  })
+    .then((response) => response.json())
+    .then((data) => {data[route_name].forEach(item => {insertList(item, myTable, route_name)})})
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Chamada de funções genéricas para carregamento inicial dos dados e geração da lista
+  --------------------------------------------------------------------------------------
+*/
+getList('tecnicas', 'myTableTecnica');
+
+getList('alunos', 'myTableAlunos');
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função genérica para colocar um item numa lista do servidor via requisição POST
+  --------------------------------------------------------------------------------------
+*/
+
+const postItem = async (item_obj, route_name, myTable) => {
+
+  item_keys = Object.keys(item_obj);
+  item_values = Object.values(item_obj);
+
+  const formData = new FormData();
+
+  for (let [key, value] of Object.entries(item_obj)) {
+    console.log(`key: ${key} value: ${value}`);
+    formData.append(key, value);
+  }
+
+  let url = 'http://127.0.0.1:5000/' + route_name;
+  fetch(url, {
+    method: 'post',
+    body: formData
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        insertList(item_obj, myTable, route_name);
+        alert(route_name + " adicionado(a)!");
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão close para cada item de lista, conforme rota
+  --------------------------------------------------------------------------------------
+*/
+const insertButton = (parent, route_name) => {
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = `close_${route_name}`;
+  span.appendChild(txt);
+  parent.appendChild(span);
+}
+
+
+/*
+  ----------------------------------------------------------------------------------------------------
+  Função genérica para remover um item de uma lista de acordo com o click no botão close conforme rota
+  ----------------------------------------------------------------------------------------------------
+*/
+const removeElement = (route_name) => {
+  let close = document.getElementsByClassName(`close_${route_name}`);
+  let i;
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement.parentElement;
+      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+      if (confirm("Você tem certeza?")) {
+        div.remove()
+        deleteItem(route_name, nomeItem)
+        alert("Removido!")
+      }
+    }
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------------------
+  Função genérica para remover um item de uma lista do servidor via requisição DELETE, conforme rota
+  --------------------------------------------------------------------------------------------------
+*/
+const deleteItem = (route_name, item) => {
+  console.log(item)
+
+  let url = 'http://127.0.0.1:5000/' + route_name + '?nome=' + item;
+  fetch(url, {
+    method: 'delete'
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter o valor do Nivel selecionado pelo usuario dentre os botões de Radio
+  --------------------------------------------------------------------------------------
+*/
+const getNivelValue = (newNivel) => {
+  var radio_btns = document.getElementsByName(newNivel), i;
+  for (i = 0; i < radio_btns.length; i++)
+    if (radio_btns[i].checked)
+      return radio_btns[i].value;
+  return null;
+}
+
+/*
+  -----------------------------------------------------------------------------------------------------------
+  Função para construir e devolver um objeto a partir dos valores fornecidos pelo usuário no form do frontend,
+  conforme rota
+  -----------------------------------------------------------------------------------------------------------
+*/
+const getItemObj = async (route_name, ...args) => {
+  if (route_name === 'tecnica') {
+    // Obter o termo fornecido pelo usuário a ser descrito pelo LLM
+    const termDescribe = document.getElementById(args[0]).value;
+    console.log(`termDescribe: ${termDescribe}`);
+        
+    // Obter o prompt conforme o termo fornecido pelo usuário
+    const prompt = getPrompt(termDescribe);
+
+    // Chamada ao componente externo OpenAI API para o LLM GPT-4o
+    loader.style.display = "block";
+    const descriptionData = await generateText(prompt);
+    loader.style.display = "none";
+
+    // Popular campo correspondente no formulário
+    const inputDescription = document.getElementById(args[1]);
+    inputDescription.value = descriptionData.choices[0]["message"]["content"]
+
+    // Chamada ao componente externo Youtube API para pesquisa de vídeo, dada um termo chave
+    loader.style.display = "block";
+    const videosData = await fetchVideos(termDescribe, 1);
+    loader.style.display = "none";
+
+    // Popular campo correspondente
+    const inputVideo = document.getElementById(args[3]);
+    inputVideo.value = `https://www.youtube.com/watch?v=` + videosData["items"][0]["id"]["videoId"];
+
+    return ({
+      'nome': document.getElementById(args[0]).value,
+      'descricao': document.getElementById(args[1]).value,
+      'nivel': getNivelValue(args[2]),
+      'video': document.getElementById(args[3]).value
+    });
+  } else if (route_name === 'aluno') {
+
+    // Obter o número de CEP fornecido pelo usuário
+    const inputEndereco = document.getElementById(args[4])
+    const CEP_number = document.getElementById(args[4]).value;
+
+    // Chamada ao componente externo viaCEP API para pesquisa de endereço, dado um CEP
+    loader.style.display = "block";
+    const enderecoData = await getCEP(CEP_number);
+    loader.style.display = "none";
+
+    // Popular campo correspondente
+    inputEndereco.value = `${enderecoData.logradouro}, ${enderecoData.bairro}, ${enderecoData.localidade}, ${enderecoData.uf}`;
+
+    return ({
+      'nome': document.getElementById(args[0]).value,
+      'data_de_nascimento': document.getElementById(args[1]).value,
+      'data_de_inicio': document.getElementById(args[2]).value,
+      'graduacao': document.getElementById(args[3]).value,
+      'endereco': inputEndereco.value
+    });
+
+  } else if (route_name === 'saude_parametros') {
+     // TODO: popular output com o resultado do modelo
+
+    return ({
+      'nome': document.getElementById(args[0]).value,
+      'age': document.getElementById(args[1]).value,
+      'sex': document.getElementById(args[2]).value,
+      'cp': document.getElementById(args[3]).value,
+      'trtbps': document.getElementById(args[4]).value,
+      'chol': document.getElementById(args[5]).value,
+      'fbs': document.getElementById(args[6]).value,
+      'restecg': document.getElementById(args[7]).value,
+      'thalachh': document.getElementById(args[8]).value,
+      'exng': document.getElementById(args[9]).value,
+      'oldpeak': document.getElementById(args[10]).value,
+      'slp': document.getElementById(args[11]).value,
+      'caa': document.getElementById(args[12]).value,
+      'thall': document.getElementById(args[13]).value
+    });    
+  } else {
+    console.log('Unknown item type!')
+  }
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função genérica para adicionar um novo item em uma tabela especifica, conforme rota
+  --------------------------------------------------------------------------------------
+*/
+const newItem = async (route_name, myTable, ...args) => {
+  const item_obj = await getItemObj(route_name, ...args);
+  if (args[0] === '') {
+    alert("Escreva o nome da entrada!");
+  } else {
+    postItem(item_obj, route_name, myTable)
+  }
+}
+
+/*
+  ----------------------------------------------------------------------------------------------
+  Função genérica para limpar campos de entrada no formulário referente a uma tabela especifica
+  ----------------------------------------------------------------------------------------------
+*/
+const clearInputFields = (myTable) => {
+  if (myTable === 'myTableTecnica') {
+    document.getElementById("newTecnica").value = "";
+    document.getElementById("newDescricao").value = "";
+    document.getElementsByName("newNivel")[0].checked = true;
+    document.getElementById("newVideo").value = "";
+  } else if (myTable === 'myTableAluno') {
+    document.getElementById("newAluno").value = "";
+    document.getElementById("newDataNascimento").value = "";
+    document.getElementById("newDataInicio").value = "";
+    document.getElementById("newGraduacao").value = "";
+    document.getElementById("newEndereco").value = "";
+  } else {
+    console.log('Unknown item type');
+  }
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função genérica para inserir itens na lista específica apresentada
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (item_obj, myTable, route_name) => {
+  let item = Object.values(item_obj);
+  let key = Object.keys(item_obj);
+
+  let table = document.getElementById(myTable);
+  let row = table.insertRow();
+
+  for (let i = 0; i < item.length; i++) {
+    let cel = row.insertCell(i);
+
+    // Se entrada for relativa a video, então aplicar hiperlink
+    if (key[i] === 'video'){
+      const link = document.createElement('a');
+      link.href = item[i];
+      link.target = '_blank';
+      link.innerText = item[i];
+      cel.appendChild(link);
+    }
+    else{
+      cel.textContent = item[i];
+    }
+  }
+  insertButton(row.insertCell(-1), route_name);
+  clearInputFields(myTable);
+  removeElement(route_name);
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para selecionar aba ativa no frontend da aplicação
+  --------------------------------------------------------------------------------------
+*/
+const setTab = (tab_content, tab_button) => {
+
+  const tabcontents = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontents.length; i++) {
+    tabcontents[i].style.display = "none";
+  }
+
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  tab_content.style.display = "block";
+  tab_button.classList.add("active");
+
+}
+
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Associando escutadores para o evento de click sobre o botão associado a cada aba
+  --------------------------------------------------------------------------------------
+*/
+
+const newTecnicaTab = document.getElementById("newTecnica_tab");
+const newTecnicaContent = document.getElementById("newTecnica_content");
+
+const newAlunoTab = document.getElementById("newAluno_tab");
+const newAlunoContent = document.getElementById("newAluno_content");
+
+
+newTecnicaTab.addEventListener("click", () => {
+  setTab(newTecnicaContent, newTecnicaTab);
+});
+
+newAlunoTab.addEventListener("click", () => {
+  setTab(newAlunoContent, newAlunoTab);
+});
+
+newTecnicaTab.click();
+
+
